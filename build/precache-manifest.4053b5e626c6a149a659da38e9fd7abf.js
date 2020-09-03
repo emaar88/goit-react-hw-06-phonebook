7b03cc2dbf66eb5bf5c976dce95b11e4ba007c75
serhii-serdiuk <sergiy_serduk@i.ua>
2020-09-03 12:08:25 +0300

    HW-6 end

---
+++ build/precache-manifest.4053b5e626c6a149a659da38e9fd7abf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3add1694b024dbec236546b4c8f4203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d5ca3bbc541b5de573c",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "379211df453f25ef8953",
+    "url": "/static/css/main.ab4b7b12.chunk.css"
+  },
+  {
+    "revision": "9d5ca3bbc541b5de573c",
+    "url": "/static/js/2.a5fa8b27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a5fa8b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379211df453f25ef8953",
+    "url": "/static/js/main.171bf103.chunk.js"
+  },
+  {
+    "revision": "5525daaeb88e7a2baf43",
+    "url": "/static/js/runtime-main.9764efd9.js"
+  }
+]);
